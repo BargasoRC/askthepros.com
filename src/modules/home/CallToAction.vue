57b0e5752fd9ceb3cf3c7fe8c37a5a7fe986d451
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-05-26 17:36:03 +0800

    calltoactionupdate

--- src/modules/home/CallToAction.vue
+++ src/modules/home/CallToAction.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="learn-how" class="sectionn">
-    <div class="container"></div>
+    <div class="container">
+      <div class="row no-gutters">
+        <span class="col-md-6">
+          <iframe class="iframe" src="https://www.youtube.com/watch?v=nY75wI85lco" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </span>
+        <span class="col-md-6">   
+            <h1>Connect your Social Media<br>and Automate.</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the. </p>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,6 +31,23 @@ export default {
   background-color: #01004e;
   min-height: 60vh;
   width: 100%;
+  color: white;
+  font-family: 'Roboto';
+}
+
+.row {
+  padding-top: 20vh;
+}
+
+h1 {
+  font-weight: 600;
+  color: #F1B814;
+  font-size: 40px;
+}
+
+p {
+  margin-top: 20px;
+  font-size: 16px;
 }
 </style>
 
