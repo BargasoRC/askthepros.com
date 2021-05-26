5004d6395f0d4656aab0ed925749518e7c0e5af3
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-05-26 16:07:58 +0800

    initial update on landing section

---
+++ src/modules/home/Subscription.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="pricing" class="sectionn">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+import ROUTER from 'src/router'
+export default {
+  methods: {
+    redirect(parameter) {
+      ROUTER.push(parameter)
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+@import '~assets/style/colors.scss';
+.sectionn {
+  background-color: white;
+  min-height: 100vh;
+  width: 100%;
+}
+</style>
+
---
+++ src/modules/home/Testimonials.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="testimonials" class="sectionn">
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+import ROUTER from 'src/router'
+export default {
+  methods: {
+    redirect(parameter) {
+      ROUTER.push(parameter)
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+@import '~assets/style/colors.scss';
+.sectionn {
+  background-color: #01004E;
+  min-height: 100vh;
+  width: 100%;
+}
+</style>
+
