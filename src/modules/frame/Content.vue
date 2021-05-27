e86d7e42da5bed457b13f9b747fba48c4b583050
BargasoRC <renanbargaso16@gmail.com>
2021-05-27 17:20:35 +0800

    added dropdown buttons

--- src/modules/frame/Content.vue
+++ src/modules/frame/Content.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="content-holder">
+    <login-header></login-header>
     <transition >
       <router-view ></router-view>
     </transition>
+    <landing-footer></landing-footer>
   </div>
 </template>
+<script>
+export default {
+  components: {
+    'login-header': () => import('modules/frame/Header2.vue'),
+    'landing-footer': () => import('modules/frame/Footer.vue')
+  }
+}
+</script>
 <style scoped>
   .content-holder{
     width: 100%;
     min-height: 400px;
     float: left;
     overflow: hidden;
+    background: linear-gradient(180deg, #fff 6.77%, rgba(255,255,255,0.782123) 19.79%, rgba(255,255,255,0.326451) 63.54%, rgba(255,255,255,0.535072) 76.04%, rgba(255,255,255,0.184593) 86.46%, rgba(255,255,255,0) 100%),url(/static/img/banner.7334e17.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
   }/*-- content-holder --*/
 </style>
---
+++ src/modules/generic/roundedSelectBtn.vue
@@ -0,0 +1,121 @@
+<template>
+  <div style="width: 100% position: relative">
+    <button
+      class="btn dialogueBTN select-btn"
+      type="button"
+      id="dropdownMenuLink"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+      :style="styles"
+    >
+      {{ 
+        selectedItem ?
+          selectedItem 
+        : 
+          selectedIndex != undefined && selectedIndex != null ?
+            items[selectedIndex]
+          :
+            placeholder 
+      }}
+    </button>
+    <div 
+      class="dropdown-menu"
+      :style="{
+        ...{
+          borderRadius: '15px',
+          overflow: 'hidden',
+          width: 'calc(100% - 35px)'
+        },
+        dropdownItemStyles
+      }"
+    >
+      <b
+        class="dropdown-item font-weight-normal select-item"
+        href="#"
+        v-for="(el, ndx) in items"
+        :key="ndx"
+        @click="selectItem(ndx, el)"
+        :style="{
+          color: (
+            dropdownItemStyles ? 
+              dropdownItemStyles.color ?
+                dropdownItemStyles.color
+              :
+                '#272727'
+            :
+              '#272727'
+          )
+        }"
+      >
+        {{ el }}
+      </b
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items', 'placeholder', 'styles', 'selectedIndex', 'dropdownItemStyles'],
+  data() {
+    return {
+      selectedItem: null
+    }
+  },
+  methods: {
+    selectItem(ndx, item) {
+      this.selectedItem = item
+      let temp = {
+        index: ndx,
+        item: item
+      }
+      this.$emit('onSelect', temp)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/style/colors.scss";
+.select-btn {
+  font: 13px sans-serif;
+  padding-left: 1em;
+  padding-right: 1.6em;
+  display: flex;
+  justify-content: space-between;
+  background: $white !important;
+  outline: none;
+}
+
+.select-btn::after {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  margin: -.3em 0 0 .4em;
+  vertical-align: middle;
+  content: "";
+  border: .3em solid;
+  border-width: 0 .15em .15em 0;
+  transform: rotateZ(45deg);
+  font-weight: 100 !important;
+}
+.select-item {
+  cursor: pointer;
+}
+.dialogueBTN{
+  outline: none !important;
+  box-shadow: none !important;
+  border: 0px;
+  border-radius: 40px;
+  font-weight: 500;
+  height: 45px;
+  padding-left: 20px;
+  padding-right: 20px;
+  min-width: 150px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid $gray !important;
+}
+</style>
