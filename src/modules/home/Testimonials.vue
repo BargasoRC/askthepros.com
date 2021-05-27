2153f8d0c1ad34837b1ac19b517bad1334d4e0a4
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-05-27 14:59:33 +0800

    Update on VideoSection, Testimonials, PricingSection

--- src/modules/home/Testimonials.vue
+++ src/modules/home/Testimonials.vue
@@ -1,12 +1,61 @@
 <template>
   <div id="testimonials" class="sectionn">
-    <div class="container"></div>
+    <div class="container">
+      <center>
+        <p id="topheader">TESTIMONIALS</p>
+        <h1>What our clients says about us.</h1>
+      </center>
+      <div class="row no-gutters holder">
+        <span class="column single-card" v-for="(item, index) in testimonials" :key="index">
+          <div class="card-image"><img id="company" :src="item.company"></div>
+          <p><i class="fas fa-quote-left"></i></p>
+          <h6>{{item.comment}}</h6>
+          <hr/>
+          <div class="row">
+          <div class="col-md-3">
+            <img id="photo" :src="item.photo">
+          </div>
+          <div class="col-md-9">
+            <p id="name">{{item.name}}</p>
+            <p id="job">{{item.job}}</p>
+          </div></div>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ROUTER from 'src/router'
 export default {
+  data(){
+    return{
+      testimonials: [
+        {
+          company: require('assets/img/temp-comp.png'),
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incig didunt ut labore et dolore magna sd gsgr gg   gdgdfg   aliqua. ',
+          photo: require('assets/img/temp-image.jpg'),
+          name: 'Juan Dela Cruz',
+          job: 'Founder, SOS Company'
+        },
+        {
+          company: require('assets/img/temp-comp1.png'),
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incig didunt ut labore et dolore magna sd gsgr gg   gdgdfg   aliqua. ',
+          photo: require('assets/img/temp-image2.jpg'),
+          name: 'Jane Millers',
+          job: 'CMO, Company.com'
+        },
+        {
+          company: require('assets/img/temp-comp2.png'),
+          comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna sed do eiusmod tempor incig didunt ut labore et dolore magna sd gsgr gg   gdgdfg   aliqua. ',
+          photo: require('assets/img/temp-image1.jpg'),
+          name: 'John Doe',
+          job: 'CEO, ABC Company'
+        }
+      ]
+    }
+
+  },
   methods: {
     redirect(parameter) {
       ROUTER.push(parameter)
@@ -21,6 +70,85 @@ export default {
   background-color: #01004E;
   min-height: 100vh;
   width: 100%;
+}
+
+center {
+  padding-top: 20vh;
+}
+
+.holder {
+  padding-top: 5vh;
+  padding-bottom: 20vh;
+}
+
+h1 {
+  font-weight: 800;
+  color: $warning;
+  font-size: 35px;
+}
+
+#topheader {
+  font-size: 24px;
+  color: $white;
+}
+p {
+  font-size: 15px;
+  color: $title;
+}
+
+.single-card {
+  background-color: $white;
+  width: 32%;
+  padding: 30px;
+  margin: 5px;
+  height: 500px;
+  border-radius: 5px;
+}
+
+#company {
+  height: 70px;
+}
+
+.card-image{
+  text-align: center;
+}
+
+p .fas {
+  color: $warning;
+  font-size: 24px;
+  margin-top: 60px;
+  text-align: left;
+}
+
+h6 {
+  font-weight: normal;
+  text-align: justify;
+  font-size: 16px;
+  min-height: 170px;
+}
+
+hr {
+  background-color: $primary;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+#photo {
+  height: 50px;
+  border-radius: 50px;
+  border: 1px solid $warning;
+}
+
+#name{
+  margin-top: 10px ;
+  font-size: 15px;
+  font-weight: bold;
+}
+
+#job{
+  font-size: 15px;
+  margin-top: -20px ;
+  font-weight: 400;
 }
 </style>
 
