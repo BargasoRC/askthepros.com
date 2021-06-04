045bc74b220ba343f5ed868cdeb5fef8aa84973f
kmlcburce <kmlcburce@gmail.com>
2021-06-04 17:06:22 +0800

    minimize header when screen changes

--- src/modules/frame/Header2.vue
+++ src/modules/frame/Header2.vue
@@ -87,6 +87,7 @@ export default {
   methods: {
     navigate(route) {
       this.$router.push(`/${route}`)
+      this.isActive = !this.isActive
     },
     headerScrollTo(id) {
       let height = Jquery(window).height()
