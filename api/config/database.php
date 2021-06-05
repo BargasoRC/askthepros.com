522d257679639bf4e59a1666f35f9b61153d4bf2
= <=>
2021-06-05 16:49:07 +0800

    update

--- api/config/database.php
+++ api/config/database.php
@@ -43,8 +43,8 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'talkfluent'),
-            'username' => env('DB_USERNAME', 'root'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
@@ -59,8 +59,7 @@ return [
                 'STRICT_TRANS_TABLES', 
                 'NO_ZERO_IN_DATE', 
                 'NO_ZERO_DATE',
-                'ERROR_FOR_DIVISION_BY_ZERO', 
-                'NO_AUTO_CREATE_USER', 
+                'ERROR_FOR_DIVISION_BY_ZERO',
                 'NO_ENGINE_SUBSTITUTION']
         ],
 
