c15b0fb46dafc138a34e4224a627aefffe7757eb
PN25-RicaChristine <ricachristine.debuayan@student.passerellesnumeriques.org>
2021-06-10 13:38:35 +0800

    Update: Content Expert Dashboard

---
+++ src/modules/dashboard/ContentExpertDashboard.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="holder">
+    <h3>Hello, Content Expert!</h3>
+    <p>Here are the latest update of activities as of today.</p>
+    <div class="col-sm-12 col-md-12 col-lg-12 d-flex justify-content-end p-0 mb-5 mt-0">
+        <roundedBtn
+            :onClick="newPost"
+            :icon="'fas fa-plus'"
+            :text="'New Post'"
+            :icon_position="'right'"
+            :styles="{
+                backgroundColor: '#01009A',
+                color: 'white'
+            }"
+        />
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-12 mt-5 mb-5 p-0">
+      <Search 
+        :category="category"
+        :activeCategoryIndex="0"
+        :activeSortingIndex="0"
+        @changeSortEvent="() => {} "
+        :grid="['list']"
+      />
+    </div>
+    <div class="col-sm-12 col-md-12 col-lg-12 mt-5 p-0 pt-5">
+      <DataTable 
+        :tableActions="tableActions"
+        :tableHeaders="tableHeaders"
+        :tableData="tableData"
+      />
+    </div>
+  </div>
+</template>
+<style scoped lang="scss" scoped>
+@import "~assets/style/colors.scss";
+.holder{
+  width: 96%;
+  margin-left: 2%;
+  margin-right: 2%;
+  float: left;
+  color: $title;
+  min-height: 100vh;
+}
+
+h3 {
+    margin-top: 25px;
+    font-weight: bold;
+    font-size: 21px;
+}
+
+p {
+  color: $text;
+} 
+.tableHeaders {
+  color: $title;
+}
+
+</style>
+<script>
+import Search from 'src/components/increment/ecommerce/filter/Product.vue'
+import DataTable from 'src/modules/generic/table'
+import roundedBtn from 'src/modules/generic/roundedBtn'
+import COLORS from 'src/assets/style/colors.js'
+import ROUTER from 'src/router'
+export default {
+  data() {
+    return {
+      colors: COLORS,
+      tableActions: [
+        {button: `<i class="fas fa-eye ml-2 mr-2" style="color: #01009A;"></i>`},
+        {button: `<i class="fas fa-pencil-alt ml-2 mr-2" style="color: #01004E;"></i>`},
+        {button: `<i class="fas fa-clone ml-2 mr-2" style="color: #01004E;"></i>`},
+        {button: `<i class="fas fa-trash-alt ml-2 mr-2" style="color: #FF0000;"></i>`}
+      ],
+      tableHeaders: [
+        {title: 'Date', key: 'created_at', type: 'text'},
+        {title: 'Post Title', key: 'title', type: 'text'},
+        {title: 'Channel Actions', key: 'channel', type: 'text'},
+        {title: 'Status', key: 'status', type: 'text'},
+        {title: 'Actions', type: 'action'}
+      ],
+      tableData: [
+        {title: 'My Post Title', channel: 'Google My Business, Facebook, Linkedin', status: 'Draft', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Linkedin', status: 'Draft', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Facebook', status: 'Published', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Facebook, Linkedin', status: 'Draft', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Linkedin', status: 'Draft', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Facebook', status: 'Published', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Facebook, Linkedin', status: 'Published', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Facebook, Linkedin', status: 'Published', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business, Linkedin', status: 'Published', created_at: '15/19/2021 at 10:31 pm'},
+        {title: 'My Post Title', channel: 'Google My Business', status: 'Published', created_at: '15/19/2021 at 10:31 pm'}
+      ],
+      category: [{
+        title: 'Sort',
+        sorting: [
+          {title: 'Created Ascending', payload: 'created_at', payload_value: 'asc', type: 'text'},
+          {title: 'Created Descending', payload: 'created_at', payload_value: 'desc', type: 'text'},
+          {title: 'Title Ascending', payload: 'title', payload_value: 'asc', type: 'text'},
+          {title: 'Title Descending', payload: 'title', payload_value: 'desc', type: 'text'},
+          {title: 'Channel Ascending', payload: 'channel', payload_value: 'asc', type: 'text'},
+          {title: 'Channel Descending', payload: 'channel', payload_value: 'desc', type: 'text'},
+          {title: 'Status Ascending', payload: 'status', payload_value: 'asc', type: 'text'},
+          {title: 'Status Descending', payload: 'status', payload_value: 'desc', type: 'text'}
+        ]
+      }]
+    }
+  },
+  components: {
+    roundedBtn,
+    Search,
+    DataTable
+  },
+  methods: {
+    newPost() {
+      console.log('New Post:::')
+    }
+  }
+}
+</script>
