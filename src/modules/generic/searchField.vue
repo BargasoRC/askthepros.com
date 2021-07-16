cb757a03605fe2758fa9483a1396009977a6c637
kmlcburce <kmlcburce@gmail.com>
2021-07-16 13:33:11 +0800

    update

---
+++ src/modules/generic/searchField.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "~assets/style/colors.scss";
+
+</style>
