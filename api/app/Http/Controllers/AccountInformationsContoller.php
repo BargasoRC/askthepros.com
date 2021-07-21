832ee0daeb35b20377f1239c69bb2c0d06b3e64b
BargasoRC <renanbargaso16@gmail.com>
2021-07-21 17:31:01 +0800

    login on enter and fixes

--- api/app/Http/Controllers/AccountInformationsContoller.php
+++ api/app/Http/Controllers/AccountInformationsContoller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Increment\Account\Models\Account;
 
 class AccountInformationsContoller extends APIController
 {
@@ -12,6 +13,28 @@ class AccountInformationsContoller extends APIController
     $user[] = app('Increment\Account\Http\AccountInformationController')->getAccountInformation($data['account_id']);
     $this->response['data'] = $user;
 
+    return $this->response();
+  }
+
+  public function updateAccountAndInformations(Request $request) {
+    $data = $request->all();
+    $result = Account::leftJoin('account_informations', 'accounts.id', '=', 'account_informations.account_id')
+            ->where('accounts.id','=', $data['account_id'])
+            ->update($data);
+    $this->response['data'] = $result;
+    return $this->response();
+  }
+
+  public function checkPassword(Request $request) {
+    $data = $request->all();
+    $result = \DB::table('accounts')
+              ->where('id', '=', $data['id'])
+              ->first();
+    if($result && \Hash::check($data['password'], $result->password)) {
+      $this->response['data'] = true;
+    }else{
+      $this->response['data'] = false;
+    }
     return $this->response();
   }
 }
