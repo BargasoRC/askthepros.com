e002a681f8255e238092118a8de47927b2862ac8
kennck <kennettecanales@gmail.com>
2021-10-11 09:43:30 +0800

    update

--- src/countries.js
+++ src/countries.js
@@ -4,22 +4,10 @@ export default{
     code: 'PH',
     currency: 'PHP',
     mobilePrefix: '+63'
-  }],
-  cities: [{
-    title: 'Cebu City',
-    region: 'Cebu',
-    country: 'Philippines'
   }, {
-    title: 'Mandaue City',
-    region: 'Cebu',
-    country: 'Philippines'
-  }, {
-    title: 'Consolacion',
-    region: 'Cebu',
-    country: 'Philippines'
-  }, {
-    title: 'Talisay',
-    region: 'Cebu',
-    country: 'Philippines'
+    title: 'USA',
+    code: 'USA',
+    currency: 'USD',
+    mobilePrefix: '+1'
   }]
 }
